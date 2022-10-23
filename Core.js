8aeb7d1dbf5ed24b3f2582784eefc6e80e7a7543
Miłosz Jakubaszek <62429578+AvengeR-13@users.noreply.github.com>
2022-10-23 20:08:03 +0200

    Added core game logic and functionalities

---
+++ Core.js
@@ -0,0 +1,31 @@
+import {Pawn} from '/src/Pawn.js'
+import {GameBoard} from '/src/GameBoard.js'
+import {Game} from '/src/Game.js'
+
+// CODE FOR TESTING
+const gameBoard = new GameBoard();
+
+//console.log(gameBoard);
+var currentPlayer = "White";
+
+// var pawn = (choosePawnToMove(findMovablePawns(findPlayerPawns(currentPlayer))));
+// console.log(getAvailableMoves(pawn))
+// Game.movePawn(pawn,chooseMove(getAvailableMoves(pawn)));
+// isGameOver();
+// console.log(...gameBoard);
+// console.log("nowa runda");
+// swapPlayers();
+// pawn = (choosePawnToMove(findMovablePawns(findPlayerPawns("Neutron"))));
+// Game.movePawn(pawn,chooseMove(getAvailableMoves(pawn)));
+// isGameOver();
+// console.log(...gameBoard);
+// pawn = (choosePawnToMove(findMovablePawns(findPlayerPawns(currentPlayer))));
+// Game.movePawn(pawn,chooseMove(getAvailableMoves(pawn)));
+// isGameOver();
+// console.log(...gameBoard);
+// console.log("nowa runda");
+// swapPlayers();
+
+
+
+//console.log(gameBoard);
